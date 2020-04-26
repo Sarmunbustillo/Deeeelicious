40867fa3cd66e929ad954965bd6643e5eaed1531
bustillo <sb@f7.de>
2020-04-26 22:10:26 +0200

    routes, store and some pug

--- app.js
+++ app.js
@@ -35,13 +35,15 @@ app.use(cookieParser());
 
 // Sessions allow us to store data on visitors from request to request
 // This keeps users logged in and allows us to send flash messages
-app.use(session({
-  secret: process.env.SECRET,
-  key: process.env.KEY,
-  resave: false,
-  saveUninitialized: false,
-  store: new MongoStore({ mongooseConnection: mongoose.connection })
-}));
+app.use(
+  session({
+    secret: process.env.SECRET,
+    key: process.env.KEY,
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  })
+);
 
 // // Passport JS is what we use to handle our logins
 app.use(passport.initialize());
@@ -50,6 +52,7 @@ app.use(passport.session());
 // // The flash middleware let's us use req.flash('error', 'Shit!'), which will then pass that message to the next page the user requests
 app.use(flash());
 
+//global
 // pass variables to our templates + all requests
 app.use((req, res, next) => {
   res.locals.h = helpers;
